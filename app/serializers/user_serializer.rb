edc380bbc5e287c0df2843292f88b454c73279f2
VitItthikon <ithikorn.amornkhana@gmail.com>
2024-11-02 14:26:11 +0700

    update attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,10 @@
 class UserSerializer
   # format json response to client
   include JSONAPI::Serializer
-  attributes :id, :email, :created_at
+  attributes :id, :email
+
+  # Define a custom attribute for the formatted creation date
+  attribute :created_date do |user|
+    user.created_at&.strftime('%d/%m/%Y')
+  end
 end
