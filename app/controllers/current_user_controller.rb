9811e6f0864396e0b971ae7ac639cb06a313cab6
VitItthikon <ithikorn.amornkhana@gmail.com>
2024-11-02 14:36:56 +0700

    add explanation

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,4 +1,5 @@
 class CurrentUserController < ApplicationController
+  # will ensure that we only see a 200 response if we have a valid JWT in the headers. If we don’t this endpoint should return a 401 status code.
   before_action :authenticate_user!
   def index
     render json: UserSerialzier.new(current_user).serializeable_hash[:data][:attributes], status: :ok
