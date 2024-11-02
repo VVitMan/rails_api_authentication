28b069609fd9c99f8c7df277063bb8f949848980
VitItthikon <ithikorn.amornkhana@gmail.com>
2024-11-02 18:05:08 +0700

    update sessions controller and user serializer

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 class Users::SessionsController < Devise::SessionsController
   include RackSessionFix
-
+  
   respond_to :json
   private
 
   def respond_with(resource, _opts = {})
     if resource.persisted?
       render json: {
-        status: {code: 200, message: 'Logged in successfully.'},
+        status: { code: 200, message: 'Logged in successfully.' },
         data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
       }, status: :ok
     else
       render json: {
-        status: {code: 401, message: 'Invalid email or password.'}
+        status: { code: 401, message: 'Invalid email or password.' }
       }, status: :unauthorized
     end
   end
@@ -32,3 +32,4 @@ class Users::SessionsController < Devise::SessionsController
     end
   end
 end
+
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer
   # format json response to client
   include JSONAPI::Serializer
   attributes :id, :email
-
+  
   # Define a custom attribute for the formatted creation date
   # operator (&.) prevents errors if created_at is nil.
   attribute :created_date do |user|
