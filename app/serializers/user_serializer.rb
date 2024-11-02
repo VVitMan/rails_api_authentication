027f2b151b01453ad96696974c718c39ce61e547
VitItthikon <ithikorn.amornkhana@gmail.com>
2024-11-02 14:28:15 +0700

    add explanation

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,6 +4,7 @@ class UserSerializer
   attributes :id, :email
 
   # Define a custom attribute for the formatted creation date
+  # operator (&.) prevents errors if created_at is nil.
   attribute :created_date do |user|
     user.created_at&.strftime('%d/%m/%Y')
   end
