7bfd39d0e58f9a41f98184710189d0b49598483a
VitItthikon <ithikorn.amornkhana@gmail.com>
2024-11-03 12:47:46 +0700

    add explanation on rack_session_fix.rb

--- app/models/concerns/rack_session_fix.rb
+++ app/models/concerns/rack_session_fix.rb
@@ -1,18 +1,31 @@
 # app/controllers/concerns/rack_session_fix.rb
-# This is a bit frustrating, as in API only mode we’re not going to be using session cookies. This is an unfixed bug in Devise with Rails 7 at the moment. There’s an issue on the Devise-JWT repo that discusses this problem including a few fixes. My pick was to go with a fix that is focused on giving devise a fake rack session hash that has enabled? set to false to avoid the error that it would otherwise raise.
+
+# This module provides a workaround for an issue with Devise in Rails 7 when using API-only mode.
+# In API-only mode, we don't use session cookies, but Devise expects a session to exist, causing errors.
+# This fix creates a "fake" Rack session to satisfy Devise's session requirements, avoiding those errors.
 module RackSessionFix
     extend ActiveSupport::Concern
+  
+    # Defines a fake Rack session class to act as a placeholder.
+    # The `enabled?` method returns false, signaling that sessions are not enabled.
     class FakeRackSession < Hash
-        def enabled?
-            false
-        end
+      def enabled?
+        false
+      end
     end
+  
+    # When this module is included, it sets up the fake session before each action.
     included do
-        before_action :set_fake_rack_session_for_devise
-        private
-        def set_fake_rack_session_for_devise
-            request.env['rack.session'] ||= FakeRackSession.new
-        end
+      before_action :set_fake_rack_session_for_devise
+  
+      private
+  
+      # Sets a fake Rack session for Devise by assigning an instance of FakeRackSession to `rack.session`.
+      # This prevents Devise from raising errors about missing sessions in API-only mode.
+      def set_fake_rack_session_for_devise
+        request.env['rack.session'] ||= FakeRackSession.new
+      end
     end
-end
+  end
+  
   
