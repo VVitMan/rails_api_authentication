d1b0539c3ee73782fb75fbd8aa616e696c69c941
VitItthikon <ithikorn.amornkhana@gmail.com>
2024-11-03 21:43:25 +0700

    create react_frontend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,6 @@
 # User model for handling user authentication and account management.
 class User < ApplicationRecord
-  # Include default Devise modules for authentication.
-  # Other optional modules available are:
-  # :confirmable - Sends emails to confirm the user's email address after registration.
-  # :lockable - Locks an account after a specified number of failed sign-in attempts.
-  # :timeoutable - Expires sessions that have been inactive for a certain duration.
-  # :trackable - Tracks sign-in count, timestamps, and IP address.
-  # :omniauthable - Adds support for third-party (OAuth) authentication.
-  # :recoverable - Sends emails to reset a forgotten password.
-  # :rememberable - Allows users to stay signed in for a long time without reauthenticating.
-
+  
   # JTIMatcher is included to ensure JWT tokens can be revoked by using a "jti" (JWT ID) attribute.
   # This strategy invalidates old tokens automatically when a new token is issued to the user.
   include Devise::JWT::RevocationStrategies::JTIMatcher
